187476ad99b8dc415e98605a57f872a542e1ad67
NetAcad DEVASC <changeme@example.com>
2025-11-28 14:27:35 +0000

    apache helicopter

---
+++ Netmiko/netmiko_show_command.py
@@ -0,0 +1,18 @@
+import datetime
+print ("Current date and time: ")
+print(datetime.datetime.now())
+print("Connecting via SSH => show version")
+#
+from netmiko import ConnectHandler
+### VAR
+
+### EXEC
+sshCli = ConnectHandler(
+    device_type="cisco_ios",
+    host="192.168.56.102",
+    port="22",
+    username="cisco",
+    password="cisco123!"
+    )
+output=sshCli.send_command("show ip int brief")
+print(output)
---
+++ ansible-apache/configure_ssh_server.sh
@@ -0,0 +1,5 @@
+sudo apt-get install openssh-server
+
+sudo apt-get install sshpass
+
+sudo apt-get start ssh
